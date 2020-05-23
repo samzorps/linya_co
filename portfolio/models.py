04b100d8f70504af306a3a7f3cf6a400c8864f30
samzorps <63939307+samzorps@users.noreply.github.com>
2020-05-23 10:14:54 -0400

    models updated

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Collection(models.Model):
+    """A collection of images
+    """
+    id = models.IntegerField()
+    image = models.ImageField()
+    date_created = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+
+
+class Image(models.Model):
+    """A single portfolio image
+    """
+    id = models.IntegerField()
+    name = models.CharField()
+    date_created = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, null=True)
+
+class Contact(models.Model):
+    """An instance of someone filling out the contact field
+    """
+    id = models.IntegerField()
+    name = models.CharField()
+    email = models.EmailField()
+    message = models.TextField()
+    time_sent = models.DateTimeField(auto_now_add=True)
