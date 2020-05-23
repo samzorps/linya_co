c76b20fc0e2ead04fc9cac6d7993134fb3571f35
samzorps <63939307+samzorps@users.noreply.github.com>
2020-05-23 18:37:53 -0400

    models created, first migation made, models registered with admin page, first template made

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,29 +1,47 @@
 from django.db import models
 from django.utils import timezone
 
-class Collection(models.Model):
+class ImageCollection(models.Model):
     """A collection of images
     """
-    id = models.IntegerField()
-    image = models.ImageField()
+    name = models.CharField(max_length=50)
+    thumbnail = models.ImageField()
     date_created = models.DateTimeField(default=timezone.now)
     description = models.TextField()
-
+    def __str__(self):
+        return self.name
 
 class Image(models.Model):
-    """A single portfolio image
+    """A single art gallery image
     """
-    id = models.IntegerField()
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     date_created = models.DateTimeField(default=timezone.now)
     description = models.TextField()
-    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, null=True)
+    collection = models.ForeignKey(ImageCollection, on_delete=models.SET_NULL,
+                                   null=True, blank=True)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+class CodeProject(models.Model):
+    """A single code project
+    """
+    name = models.CharField(max_length=50)
+    thumbnail = models.ImageField()
+    date_created = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    live_version_link = models.URLField(null=True, blank=True)
+    gitHub_link = models.URLField(null=True, blank=True)
+    def __str__(self):
+        return self.name
 
 class Contact(models.Model):
     """An instance of someone filling out the contact field
     """
-    id = models.IntegerField()
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     email = models.EmailField()
     message = models.TextField()
     time_sent = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ImageCollection, Image, CodeProject
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> Linyas Portfolio <h1>')
+    context = {
+        'image_collections' : ImageCollection.objects.order_by('date_created'),
+        'images' : Image.objects.order_by('date_created'),
+        'code_projects' : CodeProject.objects.order_by('date_created'),
+    }
+    return render(request, 'index_template.html', context)
