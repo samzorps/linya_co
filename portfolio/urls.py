49eea74aa3fb86ae579a4bb5528e9bc3a08f30ff
samzorps <63939307+samzorps@users.noreply.github.com>
2020-05-30 17:02:58 -0400

    modified templates and stylesheet

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('code', views.code_home, name='code_home'),
+    path('art', views.art_home, name='art_home')
 ]
